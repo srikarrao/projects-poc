8fee9545fff2fe568e213e749117d125542accb8
srikarrao <srikar.sirks@gmail.com>
2016-11-03 21:08:57 -0700

    new changes to the Random demo

--- CoreJavaPrograms/src/com/core/java/poc/RandomDemo.java
+++ CoreJavaPrograms/src/com/core/java/poc/RandomDemo.java
@@ -5,9 +5,23 @@ import java.util.*;
 public class RandomDemo {
 
 	public static void main(String args[]) {
-		Random randomno = new Random();
+		Random random1 = new Random(441287210);
+		for (int i = 0; i < 10; i++) {
+			System.out.print(random1.nextInt(10) + " ");
+		}
 
-		System.out.println("Next int value: " + randomno.nextInt(155));
-		System.out.println(Integer.compare(3, 5));
+		System.out.println();
+		Random random2 = new Random(-6732303926L);
+		for (int i = 0; i < 10; i++) {
+			System.out.print(random2.nextInt(10) + " ");
+		}
+		System.out.println();
+
+		int first, second;
+		for (first = 0; first <= 1; ++first) {
+			for (second = 0; second < 1; ++second) {
+				System.out.print(1 * first + second + " ");
+			}
+		}
 	}
 }
