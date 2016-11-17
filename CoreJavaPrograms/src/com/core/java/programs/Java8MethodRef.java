ce9c6d328c45c52876c30828be16893b5043a233
Srikar Rao <srikar.sirks@gmail.com>
2016-11-17 15:36:10 -0700

    Functional programming
    Functional programming

---
+++ CoreJavaPrograms/src/com/core/java/programs/Java8MethodRef.java
@@ -0,0 +1,30 @@
+package com.java.functional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Java8MethodRef {
+
+	public static void main(String args[]) {
+
+		final Consumer<String> x = new Consumer<String>() {
+			@Override
+			public void accept(String x) {
+				if (x != "M") {
+					System.out.println(x);
+				}
+			}
+		};
+
+		List<String> names = new ArrayList<String>();
+
+		names.add("M");
+		names.add("S");
+		names.add("R");
+		names.add("V");
+		names.add("K");
+		names.forEach(x);
+
+	}
+}
