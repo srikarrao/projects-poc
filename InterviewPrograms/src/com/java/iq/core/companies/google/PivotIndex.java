9b1fc8ea9d841f6d27e3ab38b90d604311d1302b
Srikar Rao <srikar.sirks@gmail.com>
2017-03-06 06:49:23 -0700

    Pivot Index

---
+++ InterviewPrograms/src/com/java/iq/core/companies/google/PivotIndex.java
@@ -0,0 +1,37 @@
+package com.java.iq.core.companies.google;
+
+public class PivotIndex {
+
+	/**
+	 * Time Complexity: O(n)
+	 * 
+	 * Space Complexity: O(1)
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public static int findIndex(int[] arr) {
+
+		if (arr == null || arr.length == 0) {
+			return -1;
+		}
+
+		int leftSum = 0;
+		int rightSum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			rightSum += arr[i];
+
+		}
+
+		for (int j = 0; j < arr.length; j++) {
+			rightSum -= arr[j];
+			if (leftSum == rightSum) {
+				System.out.println(j);
+				return j;
+			}
+			leftSum += arr[j];
+		}
+		return -1;
+	}
+}
