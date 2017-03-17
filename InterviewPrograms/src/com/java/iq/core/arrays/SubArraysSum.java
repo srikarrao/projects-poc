c61233e84efa9120aab71dbb5347546882a565f3
Srikar Rao <srikar.sirks@gmail.com>
2017-03-16 17:09:59 -0700

    Add files via upload

---
+++ InterviewPrograms/src/com/java/iq/core/arrays/SubArraysSum.java
@@ -0,0 +1,34 @@
+package com.temp.arrays;
+
+public class SubArraysSum {
+
+	public static void main(String[] args) {
+
+		int[] arr1 = { 6, 7, 8 };
+		System.out.println((int) findSubArraySum(arr1));
+
+		int[] arr2 = { 6 };
+		System.out.println((int) findSubArraySum(arr2));
+
+		int[] arr3 = { 0, 0, 0 };
+		System.out.println((int) findSubArraySum(arr3));
+
+		int[] arr4 = { 7, 9, 6, 10, 4 };
+		System.out.println((int) findSubArraySum(arr4));
+
+		int[] arr5 = { 1, 2, 3, 4 };
+		System.out.println((int) findSubArraySum(arr5));
+	}
+
+	private static double findSubArraySum(int[] arr) {
+		double sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+
+		sum = (int) sum * Math.pow(2, arr.length - 1);
+		return sum;
+	}
+
+}
