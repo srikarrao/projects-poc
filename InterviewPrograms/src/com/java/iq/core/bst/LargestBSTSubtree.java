f2aa55e6111b5e2be75a72fc8b6c7ee82442e4ab
Srikar Rao <srikar.sirks@gmail.com>
2017-05-05 16:43:59 -0700

    BST Largest subtree
    BST Largest subtree

--- InterviewPrograms/src/com/java/iq/core/bst/LargestBSTSubtree.java
+++ InterviewPrograms/src/com/java/iq/core/bst/LargestBSTSubtree.java
@@ -1,5 +1,4 @@
 package com.java.iq.core.bst;
-
 /**
  * Source:
  * http://www.geeksforgeeks.org/find-the-largest-subtree-in-a-tree-that-is
@@ -18,96 +17,73 @@ public class LargestBSTSubtree {
 
 		Node(int d) {
 			data = d;
-			left = right = null;
 		}
 	}
 
 	private static class Value {
+		int size;
+		boolean isBST;
+		int min;
+		int max;
 
-		int max_size = 0;
-		boolean is_bst = false;
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
+		Value(int size, boolean isBST, int min, int max) {
+			this.size = size;
+			this.isBST = isBST;
+			this.min = min;
+			this.max = max;
+		}
 	}
 
-	private static class BinaryTree {
+	private Value largestBST(Node node) {
 
-		private Node root;
-		Value val = new Value();
-
-		int largestBST(Node node) {
-
-			largestBSTUtil(node, val, val, val, val);
-
-			return val.max_size;
+		if (node == null) {
+			return new Value(0, true, Integer.MAX_VALUE, Integer.MIN_VALUE);
 		}
 
-		int largestBSTUtil(Node node, Value min_ref, Value max_ref,
-				Value max_size_ref, Value is_bst_ref) {
+		Value leftVal = largestBST(node.left);
+		Value rightVal = largestBST(node.right);
 
-			if (node == null) {
-				is_bst_ref.is_bst = true;
-				return 0;
-			}
+		int min = node.data;
+		int max = node.data;
+		int maxSize = 0;
 
-			int min = Integer.MAX_VALUE;
-
-			boolean left_flag = false;
-			boolean right_flag = false;
-
-			int ls, rs;
-			max_ref.max = Integer.MIN_VALUE;
-
-			ls = largestBSTUtil(node.left, min_ref, max_ref, max_size_ref,
-					is_bst_ref);
-			if (is_bst_ref.is_bst == true && node.data > max_ref.max) {
-				left_flag = true;
-			}
-			min = min_ref.min;
-			min_ref.min = Integer.MAX_VALUE;
-			rs = largestBSTUtil(node.right, min_ref, max_ref, max_size_ref,
-					is_bst_ref);
-			if (is_bst_ref.is_bst == true && node.data < min_ref.min) {
-				right_flag = true;
-			}
-
-			if (min < min_ref.min) {
-				min_ref.min = min;
-			}
-			if (node.data < min_ref.min) {
-				min_ref.min = node.data;
-			}
-			if (node.data > max_ref.max) {
-				max_ref.max = node.data;
-			}
-
-			if (left_flag && right_flag) {
-				if (ls + rs + 1 > max_size_ref.max_size) {
-					max_size_ref.max_size = ls + rs + 1;
-				}
-				return ls + rs + 1;
-			} else {
-				is_bst_ref.is_bst = false;
-				return 0;
-			}
+		if (verifyFormsBST(leftVal, rightVal, node)) {
+			min = Math.min(leftVal.min, node.data);
+			max = Math.max(rightVal.max, node.data);
+			maxSize = Math.max(maxSize, 1 + leftVal.size + rightVal.size);
+			return new Value(maxSize, true, min, max);
 		}
 
+		maxSize = Math.max(maxSize, Math.max(leftVal.size, rightVal.size));
+		return new Value(maxSize, false, min, max);
+	}
+
+	private boolean verifyFormsBST(Value left, Value right, Node parent) {
+
+		if (left.isBST && right.isBST && left.max < parent.data
+				&& right.min > parent.data) {
+			return true;
+		}
+
+		return false;
 	}
 
 	public static void main(String[] args) {
 
-		BinaryTree tree = new BinaryTree();
-		tree.root = new Node(50);
-		tree.root.left = new Node(10);
-		tree.root.right = new Node(60);
-		tree.root.left.left = new Node(5);
-		tree.root.left.right = new Node(20);
-		tree.root.right.left = new Node(55);
-		tree.root.right.left.left = new Node(45);
-		tree.root.right.right = new Node(70);
-		tree.root.right.right.left = new Node(65);
-		tree.root.right.right.right = new Node(80);
-		System.out.println("Size of largest BST is "
-				+ tree.largestBST(tree.root));
+		LargestBSTSubtree largestSubTree = new LargestBSTSubtree();
+
+		Node root = new Node(50);
+		root.left = new Node(10);
+		root.right = new Node(60);
+		root.left.left = new Node(5);
+		root.left.right = new Node(20);
+		root.right.left = new Node(55);
+		root.right.left.left = new Node(45);
+		root.right.right = new Node(70);
+		root.right.right.left = new Node(65);
+		root.right.right.right = new Node(80);
+
+		int res = largestSubTree.largestBST(root).size;
+		System.out.println("Size of largest BST is " + res);
 	}
 }
