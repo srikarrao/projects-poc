77e1c4087a8855337c03d9c56d756ce440c9bee8
srikarrao <srikar.sirks@gmail.com>
2019-02-15 00:33:05 -0700

    Made changes to PrefixSearch to support prefix of any length.

--- InterviewPrograms/src/com/java/iq/core/bsearch/PrefixSearch.java
+++ InterviewPrograms/src/com/java/iq/core/bsearch/PrefixSearch.java
@@ -17,6 +17,10 @@ public class PrefixSearch {
 		Assert.assertEquals("Index 1", 1, search(new String[] { "Kohli", "Pietersen" }, "P"));
 		Assert.assertEquals("Size 1. Found", 0, search(new String[] { "Ponting" }, "P"));
 		Assert.assertEquals("Size 1. Not Found", -1, search(new String[] { "Root" }, "P"));
+		Assert.assertEquals("Size N. Found", 1, search(new String[] { "ab", "abd", "abdf", "abz" }, "abd"));
+		Assert.assertEquals("Size N. Found", 3, search(new String[] { "ab", "abd", "abdf", "abz" }, "abz"));
+		Assert.assertEquals("Size N. Found", 2, search(new String[] { "ab", "abd", "abdf", "abz" }, "abdf"));
+		Assert.assertEquals("Size N. Not Found", -1, search(new String[] { "ab", "abd", "abdf", "abz" }, "xab"));
 	}
 
 	private static int search(String[] words, String searchPrefix) {
@@ -26,13 +30,21 @@ public class PrefixSearch {
 		}
 		int low = 0;
 		int high = words.length - 1;
+		int searchPrefixLength = searchPrefix.length();
 
 		while (low <= high) {
 			int mid = low + (high - low) / 2;
-			String prefix = words[mid].substring(0, 1);
-			if (prefix.compareTo(searchPrefix) == 0) {
+
+			String word = words[mid];
+			int compare = -1;
+
+			if (searchPrefixLength <= word.length()) {
+				compare = word.substring(0, searchPrefixLength).compareTo(searchPrefix);
+			}
+
+			if (compare == 0) {
 				return mid;
-			} else if (prefix.compareTo(searchPrefix) > 0) {
+			} else if (compare > 0) {
 				high = mid - 1;
 			} else {
 				low = mid + 1;
