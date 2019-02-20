6e17276ccb0b9ce62c994ddf1ecd0679eb4a825e
srikarrao <srikar.sirks@gmail.com>
2019-02-19 23:55:35 -0700

    Closed the stream in word count

--- InterviewPrograms/src/com/java/iq/core/companies/zynga/WordCount.java
+++ InterviewPrograms/src/com/java/iq/core/companies/zynga/WordCount.java
@@ -7,24 +7,29 @@ public class WordCount {
 	public static void main(String[] args) throws FileNotFoundException {
 		String fileName = "input-files/strText.txt";
 		Scanner input = new Scanner(new File(fileName));
-
-		// count occurrences
-		int maxChar = 0;
 		String maxStrng = "";
-		Map<String, Integer> wordCounts = new TreeMap<String, Integer>();
-		while (input.hasNext()) {
-			String next = input.next().toLowerCase();
-			if (!wordCounts.containsKey(next)) {
-				wordCounts.put(next, 1);
-			} else {
-				wordCounts.put(next, wordCounts.get(next) + 1);
+
+		try {
+			// count occurrences
+			int maxChar = 0;
+			Map<String, Integer> wordCounts = new TreeMap<String, Integer>();
+			while (input.hasNext()) {
+				String next = input.next().toLowerCase();
+				if (!wordCounts.containsKey(next)) {
+					wordCounts.put(next, 1);
+				} else {
+					wordCounts.put(next, wordCounts.get(next) + 1);
+				}
+				maxChar = Math.max(wordCounts.get(next), maxChar);
+				if (maxChar == next.length()) {
+					maxStrng = next;
+				}
 			}
-			maxChar = Math.max(wordCounts.get(next), maxChar);
-			if (maxChar == next.length()) {
-				maxStrng = next;
+		} catch (Exception e) {
+			if (input != null) {
+				input.close();
 			}
 		}
-
 		System.out.println("maximum occurred word is: " + maxStrng);
 	}
 }
