32efe3a8a9748f7a0d4b48c88283a9622a401ecb
srikarrao <srikar.sirks@gmail.com>
2019-05-31 19:33:09 -0700

    HIndex Optimized

--- InterviewPrograms/src/com/java/iq/core/companies/amazon/HIndex.java
+++ InterviewPrograms/src/com/java/iq/core/companies/amazon/HIndex.java
@@ -12,9 +12,14 @@ public class HIndex {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 4, 6, 9, 2, 1, 2};
+		int[] arr2 = { 3, 0, 6, 1, 5 };
+		int[] arr1 = { 4, 6, 9, 2, 1, 2 };
 
-		System.out.println(hIndex(arr));
+		System.out.println(hIndexOptimized(arr2));
+		System.out.println(hIndexOptimized(arr1));
+
+		System.out.println(hIndex(arr2));
+		System.out.println(hIndex(arr1));
 	}
 
 	public static int hIndex(int[] citations) {
@@ -28,4 +33,23 @@ public class HIndex {
 
 		return result;
 	}
+
+	public static int hIndexOptimized(int[] citations) {
+		int l = citations.length;
+		int[] bucket = new int[l + 1];
+		for (int i : citations) {
+			if (i > l)
+				bucket[l]++;
+			else
+				bucket[i]++;
+		}
+		int sum = 0;
+		for (int i = l; i >= 0; i--) {
+			sum += bucket[i];
+			if (sum >= i)
+				return i;
+		}
+		return 0;
+	}
+
 }
